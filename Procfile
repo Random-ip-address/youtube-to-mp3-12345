ef59d8a1227c355af99dde97abf07918fcd7b65c
ashutosh2468 <ashutoshkumarkarn01@gmail.com>
2021-04-29 10:50:19 +0530

    first inisilit

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn eleventh.wsgi
--- eleventh/settings.py
+++ eleventh/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,9 +24,9 @@ STATIC_DIR = os.path.join(BASE_DIR,'static')
 SECRET_KEY = '0lz6%2$t^oezcom36^gzq^$xb6o8l-7loh+q$!r55va#&w*^8#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -124,7 +123,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 # Activate Django-Heroku.
-django_heroku.settings(locals())
 STATICFILES_DIRS = [
 
  STATIC_DIR,
