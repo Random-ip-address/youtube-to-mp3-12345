9a425ec73cca932953dfde8699a87f258e9e12ed
ashutosh2468 <ashutoshkumarkarn01@gmail.com>
2021-04-30 21:22:03 +0530

    finel2

--- ydownloader/urls.py
+++ ydownloader/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from ydownloader import views
 
 urlpatterns = [
-	url(r'^home/$',views.greetings),
+	url(r'^https://youtube-to-mp3-convert.herokuapp.com/$',views.greetings),
     url(r'^home/download/$',views.download),
     url(r'^home/downloading/$',views.downloading),
     
