0f6663952dde051346a9926abc656973f4f050b7
ashutosh2468 <ashutoshkumarkarn01@gmail.com>
2021-04-30 21:15:23 +0530

    home mod

--- eleventh/urls.py
+++ eleventh/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url('',include('ydownloader.urls')),
+    url('https://youtube-to-mp3-convert.herokuapp.com/',include('ydownloader.urls')),
     path('admin/', admin.site.urls),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
