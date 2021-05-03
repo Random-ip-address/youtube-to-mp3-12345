2859203f0a56e4e223bd7941159f4021838caa24
ashutosh2468 <ashutoshkumarkarn01@gmail.com>
2021-05-03 20:52:51 +0530

    bug fix 4

--- ydownloader/urls.py
+++ ydownloader/urls.py
@@ -2,8 +2,15 @@ from django.conf.urls import url
 from ydownloader import views
 
 urlpatterns = [
-	url(r'^home/$',views.greetings),
-    url(r'^home/download/$',views.download),
-    url(r'^home/downloading/$',views.downloading),
+	url(r'^ $',views.greetings),
+    url(r'^ /download/$',views.download),
+    url(r'^ /downloading/$',views.downloading),
     
  ]
+
+
+
+#  url(r'^home/$',views.greetings),
+#     url(r'^home/download/$',views.download),
+#     url(r'^home/downloading/$',views.downloading),
+    
